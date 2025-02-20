0aa707d37f00883b85715757063ba2e22f4d635d
Yash Kambaria <yashkambaria245@gmail.com>
2025-02-20 11:20:06 +0530

    yml file updated

--- journalApp/src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class JournalApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JournalApplication.class, args);
     }
