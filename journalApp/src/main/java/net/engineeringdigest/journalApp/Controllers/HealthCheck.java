6ab71e8eede49fbfa62b31cee6619b530a605c98
Yash Kambaria <yashkambaria245@gmail.com>
2025-03-26 13:57:26 +0530

    bug fixed

--- journalApp/src/main/java/net/engineeringdigest/journalApp/Controllers/HealthCheck.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/Controllers/HealthCheck.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheck {
 	
 	@GetMapping("/check")
-	public String check(){
+	public String check() {
 		return "working fine";
 	}
 	
